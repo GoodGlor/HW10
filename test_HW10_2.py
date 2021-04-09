cc824beb0256f00372ead7561b4126f8b1593728
Levko <artem.levkovich0011@gmail.com>
2021-04-09 11:01:35 +0300

    fix bag testing in task 2 and 3

--- test_HW10_2.py
+++ test_HW10_2.py
@@ -7,22 +7,22 @@ from unittest.mock import patch
 class TestRegistration(unittest.TestCase):
     reg = Registration()
 
-    def test_registred_user(self):
+    def test_1registred_user(self):
         self.user_test = ['Jon', 'jon@gmail.com', '1234q']
         with patch('builtins.input', side_effect=self.user_test):
             self.reg.registred()
 
-    def test_name_already_exist(self):
+    def test_2name_already_exist(self):
         self.user_name = 'Jon'
         with mock.patch('builtins.input', return_value=self.user_name):
             assert self.reg.user_name() == False
 
-    def test_email_already_exist(self):
+    def test_3email_already_exist(self):
         self.user_email = 'jon@gmail.com'
         with mock.patch('builtins.input', return_value=self.user_email):
             assert self.reg.user_email() == False
 
-    def test_user_name(self):
+    def test_4user_name(self):
         names_false = ['asd123', '123456', '@!&^%', 1234, 1.2123, ' ', 'Андній']
         for name_false in names_false:
             with mock.patch('builtins.input', return_value=name_false):
@@ -33,7 +33,7 @@ class TestRegistration(unittest.TestCase):
             with mock.patch('builtins.input', return_value=name_true):
                 assert self.reg.user_name() == True
 
-    def test_user_email(self):
+    def test_5user_email(self):
         emails_false = ['artem@', ' ', '.com', 'ASDFGHSD.COM']
         for email_false in emails_false:
             with mock.patch('builtins.input', return_value=email_false):
@@ -44,7 +44,7 @@ class TestRegistration(unittest.TestCase):
             with mock.patch('builtins.input', return_value=email_true):
                 assert self.reg.user_email() == True
 
-    def test_user_pasw(self):
+    def test_6user_pasw(self):
         passwords_false = ['\tasd1@asd', ' asdd asdasd\t', '\t', ' ', 'rtey  fgh', 'фівфівй']
         for password_false in passwords_false:
             with mock.patch('builtins.input', return_value=password_false):
--- test_HW10_3.py
+++ test_HW10_3.py
@@ -1,4 +1,5 @@
 from HW10_3 import Authorization
+from HW10_2 import Registration
 import unittest
 from unittest.mock import patch
 
@@ -6,9 +7,15 @@ from unittest.mock import patch
 
 
 class TestAuthorization(unittest.TestCase):
+    reg = Registration()
+
+    def test_0registred_user(self):
+        self.user_test = ['Jon', 'jon@gmail.com', '1234q']
+        with patch('builtins.input', side_effect=self.user_test):
+            self.reg.registred()
 
 
-    def test_check_email(self):
+    def test_1check_email(self):
         emails = ['email@asd', 1234, 'ASDASD@GMAIL.COM', 1.123, '123.123@MAIL.UA']
         for email in emails:
             with patch('builtins.input', return_value=email):
@@ -16,7 +23,7 @@ class TestAuthorization(unittest.TestCase):
                 assert auth.check_email() == False
 
 
-    def test_check_password(self):
+    def test_2check_password(self):
         password1 = {'jon@gmail.com':'email@asd'}
         password2 ={'jon@gmail.com': 12}
         password3 = {'jon@gmail.com':'ASDASD@GMAIL.COM'}
@@ -31,7 +38,7 @@ class TestAuthorization(unittest.TestCase):
 
 
 
-    def test_authorization(self):
+    def test_3authorization(self):
         self.email = ['jon@gmail.com', '1234q']
         with patch('builtins.input', side_effect=self.email):
             auth = Authorization()
